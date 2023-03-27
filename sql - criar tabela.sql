d8fa15bc1966c03573e0e217a43c33fa148775df
Gustavo Henrique Ferreira de Miranda Oliveira <guga.h.f.m.o@gmail.com>
2023-03-27 12:28:08 -0300

    up

---
+++ sql - criar tabela.sql
@@ -0,0 +1,12 @@
+create database agenda;
+show databases;
+use agenda;
+
+create table contatos(
+	idcon int primary key auto_increment,
+	nome varchar(50) not null,
+	fone varchar(15) not null,
+	email varchar(50)
+);
+show tables;
+describe contatos;
